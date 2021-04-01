8c373904ec7a0e0400ea42110edce9fbfc9354a3
Valerie Cevallos <valerie.cevallos@gmail.com>
2021-04-01 15:21:20 -0400

    fixed session

--- app.js
+++ app.js
@@ -45,11 +45,6 @@ app.use(
       cookie: {
         maxAge: 7200000 //120 min (120 * 60 * 1000ms) 
       },
-      store: new MongoStore({
-        mongooseConnection: mongoose.connection,
-        // ttl ==> time to live
-        ttl: 24 * 60 * 60 // 1 day
-      })
   })
 );
 
